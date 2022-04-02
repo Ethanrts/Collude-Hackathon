7a642e20fa76dee7f3623872fa211acf42a9550a
amnehalsuqi <aalsu013@ucr.edu>
2022-04-02 11:31:41 -0700

    firebase login

--- auth1.py
+++ auth1.py
@@ -8,10 +8,37 @@ firebaseConfig = {
   'storageBucket': "collude-58309.appspot.com",
   'messagingSenderId': "547641500704",
   'appId': "1:547641500704:web:4c91263a884dd6fd960845",
-  'measurementId': "G-DMVZKWYXQN"}
+  'measurementId': "G-DMVZKWYXQN",
+  'databaseURL': ""}
 
 #connect to firebase acc
 firebase=pyrebase.initialize_app(firebaseConfig)
 
 auth=firebase.auth()
 
+def signup():
+  email=input("Enter Email: ")
+  password=input("Enter Password: ")
+  try:
+
+    user=auth.create_user_with_email_and_password(email,password)
+    print("Success")
+  except:
+    print("TRY AGAIN")
+
+def login():
+  email = input("Enter Email: ")
+  password = input("Enter Password: ")
+  try:
+    user = auth.sign_in_with_email_and_password(email, password)
+    print("Success")
+  except:
+    print("TRY AGAIN")
+
+ans=input("New user? y/n")
+
+if ans == 'y':
+  signup()
+
+elif ans== 'n':
+  login()
