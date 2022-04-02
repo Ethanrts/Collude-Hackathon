441cc0c42ef5103a5646d9a92034be981078793f
amnehalsuqi <aalsu013@ucr.edu>
2022-04-02 14:23:54 -0700

    auth DB

--- creaet_db.py
+++ creaet_db.py
@@ -10,6 +10,17 @@ firestore_db = firestore.client()
 # add data
 #firestore_db.collection(u'List').add({'Category': 'Dairy', 'Product Name': '2% Milk', 'Quantity': 2})
 # read data
+
+category_string = ''
+product_name = 'Almond Milk'
+quantity_var = ''
+firestore_db.collection(u'Grocery_List').add({'Category': 'Dairy', 'Product Name': product_name, 'Quantity': 1})
+
+
 snapshots = list(firestore_db.collection(u'Grocery_List').get())
 for snapshot in snapshots:
     print(snapshot.to_dict())
+
+
+
+
