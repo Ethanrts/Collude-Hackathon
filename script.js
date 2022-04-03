c9738b646eb3952249f216a0ecd747dbf531235e
Sakhee-Parekh <sakhee@vjpsolutions.com>
2022-04-03 02:50:56 -0700

    resultpage

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function submit() {
     let pw = document.getElementById('password').value;
 
     console.log(em);
-    fetch('https://c1dc-169-235-64-155.ngrok.io/login?email=' + em + '&password=' + pw)
+    fetch('https://1e5f-169-235-64-155.ngrok.io/login?email=' + em + '&password=' + pw)
         .then(response => response.json())
         .then(function(response) {
             if (response.status) {
@@ -32,7 +32,7 @@ function submit2() {
     console.log(emi);
     console.log(pwa);
     console.log(pwa2);
-    fetch('https://c1dc-169-235-64-155.ngrok.io/sign_up?email=' + emi + '&password=' + pwa + '&password2=' + pwa2)
+    fetch('https://1e5f-169-235-64-155.ngrok.io/sign_up?email=' + emi + '&password=' + pwa + '&password2=' + pwa2)
         .then(response => response.json())
         .then(function(response) {
             if (response.status) {
@@ -50,8 +50,19 @@ function submit2() {
 }
 
 function load() {
-    // groceries = ['eg', 'sgtek']
-    // read the groceries in the url
+    fetch('https://1e5f-169-235-64-155.ngrok.io/list')
+        .then(response => response.json())
+        .then(function(response) {
+            console.log(response)
+            document.getElementById('display').innerHTML = '';
+            Object.keys(response).forEach(function(category) {
+                document.getElementById('display').innerHTML += '<div> <p><strong>' + category + '</strong></p>';
+                response[category].forEach(function(item) {
+                    document.getElementById('display').innerHTML += '<p onclick="add(`' + item + '`)">' + item + '</p>';
+                });
+                document.getElementById('display').innerHTML += '</div>';
+            })
+        })
     document.getElementById('display').innerHTML = '<div class="lists">'
 
     for (let i = 0; i < groceries.length; i++) {
@@ -59,5 +70,37 @@ function load() {
     }
 
     document.getElementById('display').innerHTML += '</div>'
+
+}
+clickedStuff = []
+
+function add(item) {
+    clickedStuff.push(item);
+    console.log('user clicked', clickedStuff);
+}
+
+function submit3() {
+    // fetch('https://1e5f-169-235-64-155.ngrok.io/result')
+    //     .then(response => response.json())
+    //     .then(function(response) {
+    //         for (i = 0; i < clickedStuff.length; i++) {
+    //             document.write(clickedStuff[i] + "<br />");
+    //         }
+
+    //     })
+    window.location = './result.html?cart=' + JSON.stringify(clickedStuff);
+}
+
+function resultLoad() {
+    const queryString = window.location.search;
+
+    const urlParams = new URLSearchParams(queryString);
+    const flist = JSON.parse(urlParams.get('cart'))
+
+    console.log(flist)
+    flist.forEach(function(ingredient) {
+
+        document.getElementById('flist').innerHTML += '<div>' + ingredient + '</div>'
+    })
 
 }
